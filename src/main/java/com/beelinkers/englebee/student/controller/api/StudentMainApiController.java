f78ae3921f5b6fbb0ad707d4a1e9366b1beeb4f1
yeong1203 <cozyluv117@gmail.com>
2024-08-20 18:28:02 +0900

    feat: student main page 관련 API Controller 구현

--- src/main/java/com/beelinkers/englebee/student/controller/api/StudentMainApiController.java
+++ src/main/java/com/beelinkers/englebee/student/controller/api/StudentMainApiController.java
@@ -1,9 +1,14 @@
 package com.beelinkers.englebee.student.controller.api;
 
+import com.beelinkers.englebee.general.controller.page.MainPageController;
+import com.beelinkers.englebee.student.dto.response.StudentMainPageDTO;
 import com.beelinkers.englebee.student.service.StudentMainService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -13,4 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentMainApiController {
 
   private final StudentMainService studentMainService;
+
+  @GetMapping("/qna")
+  public ResponseEntity<StudentMainPageDTO> getMainPage(
+          @RequestParam("memberSeq") Long memberSeq,
+          @RequestParam("code") String code
+  ) {
+    StudentMainPageDTO studentMainPageDTO = studentMainService.getStudentMainPage(memberSeq, code);
+    return ResponseEntity.ok(studentMainPageDTO);
+  }
+
 }
