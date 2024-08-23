40b9bce3d2e0530714953deb1a73556106e2d2f4
wonjun <wonjon0930@daum.net>
2024-08-23 23:45:02 +0900

    feat: 외부 mariadb 연결 되었는지 확인

--- src/main/java/com/beelinkers/englebee/DBConnectionTestController.java
+++ src/main/java/com/beelinkers/englebee/DBConnectionTestController.java
@@ -1,14 +1,19 @@
 package com.beelinkers.englebee;
 
+import com.beelinkers.englebee.auth.domain.entity.Member;
+import com.beelinkers.englebee.auth.domain.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DBConnectionTestController {
+  @Autowired
+  private MemberRepository memberRepository;
 
   @GetMapping("/db/conn")
-  public void testDB(){
-
+  public Member testDB(){
+    return memberRepository.findById(1L).orElse(null);
   }
 
 }
